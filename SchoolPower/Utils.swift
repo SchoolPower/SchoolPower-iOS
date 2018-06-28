6d186a0092f1db0a3dfbf362a65b2ed23dd53ed3
CarbonylGroup <carbonylgp@gmail.com>
2018-06-28 08:50:19 +0800

    [Fix] Fix crash when letter grade not found

--- SchoolPower/Utils.swift
+++ SchoolPower/Utils.swift
@@ -83,7 +83,13 @@ class Utils {
     ]
     
     static func indexOfString (searchString: String, domain: Array<String>) -> Int {
-        return domain.index(of: searchString)!
+        print("[][]["+searchString)
+        if domain.index(of: searchString) != nil {
+            return domain.index(of: searchString)!
+        } else {
+            // return the "--" color if the letter grade is not found
+            return 7
+        }
     }
 }
 
